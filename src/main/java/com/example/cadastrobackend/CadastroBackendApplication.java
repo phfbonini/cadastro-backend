f5cb2cd43e95e5e348ac00f2d3aa45716dc82c97
Pedro Bonini <86377602+phfbonini@users.noreply.github.com>
2024-03-22 18:33:48 -0300

    commit inicial

---
+++ src/main/java/com/example/cadastrobackend/CadastroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadastrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroBackendApplication.class, args);
+	}
+
+}
